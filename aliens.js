37ee3c694d68fe33b7ea9d8a13ddc712a0ad16dd
Sagar Kalbande <sagarkalbande1993@gmail.com>
2017-02-27 10:48:00 +0530

    Add files via upload

--- aliens.js
+++ aliens.js
@@ -1,44 +1,46 @@
 function alien(){
-	this.x = random()*width;
+	this.x = random(-400,width);
 	this.y = random(0,-600);
 
 
 	this.update = function(){
 		this.y = this.y + 10;
+		this.x = this.x + 3;
 	}
 
 	this.show = function(){
 		fill(255);
-		rect(this.x,this.y,1,15);
+		rect(this.x,this.y,0.2,15);
 	}
 
 	this.restore = function(){
 		if(this.y>600){
 			this.y=random(0,-600);
-			this.x=random()*width;
+			this.x=random(-400,width);
 		}
 
 	}
 }
 
 function alien1(){
-	this.x = random()*width;
+	this.x = random(-400,width);
 	this.y = random(0,-600);
 
 
 	this.update = function(){
 		this.y = this.y + 5;
+		this.x = this.x + 3;
 	}
 
 	this.show = function(){
 		fill(255);
-		rect(this.x,this.y,1,8);
+		rect(this.x,this.y,0.2,8);
 	}
 
 	this.restore = function(){
 		if(this.y>600){
 			this.y=random(0,-600);
-			this.x=random()*width;
+			this.x=random(-400,width);
 		}
 
 	}
