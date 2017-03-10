c3300fa0e28de2a0bac2e3d738f48d5eb9583f69
Sagar Kalbande <sagarkalbande1993@gmail.com>
2017-03-10 22:40:36 +0530

    Add files via upload

--- aliens.js
+++ aliens.js
@@ -1,47 +1,28 @@
 function alien(){
+
+	this.speedx = 5;
+	this.speedy = 10*random(0,0.5);
+	
 	this.x = random(-400,width);
 	this.y = random(0,-600);
 
 
 	this.update = function(){
-		this.y = this.y + 10;
-		this.x = this.x + 3;
+		this.y = this.y + this.speedy + 5;
+		this.x = this.x + 2;
 	}
 
 	this.show = function(){
 		fill(255);
-		rect(this.x,this.y,0.2,15);
+		rect(this.x,this.y,0.5,5*this.speedy);
 	}
 
 	this.restore = function(){
 		if(this.y>600){
 			this.y=random(0,-600);
-			this.x=random(-400,width);
+			this.x=random(-1000,width);
 		}
 
 	}
 }
 
-function alien1(){
-	this.x = random(-400,width);
-	this.y = random(0,-600);
-
-
-	this.update = function(){
-		this.y = this.y + 5;
-		this.x = this.x + 3;
-	}
-
-	this.show = function(){
-		fill(255);
-		rect(this.x,this.y,0.2,8);
-	}
-
-	this.restore = function(){
-		if(this.y>600){
-			this.y=random(0,-600);
-			this.x=random(-400,width);
-		}
-
-	}
-}
--- sketch.js
+++ sketch.js
@@ -1,17 +1,12 @@
 var al = [];
-var al1 = [];
+
 
 function setup(){
 
 createCanvas(1200,600);
 
-	for (var i = 0; i < 500; i++) {
+	for (var i = 0; i < 1000; i++) {
 		al[i] = new alien();
-	}
-	
-
-	for (var i = 0; i < 500; i++) {
-		al1[i] = new alien1();
 	}
 }
 
@@ -19,19 +14,11 @@ function draw(){
 
 background(135,206,250);
 
-for (var i = 0; i < 500; i++) {
+
+	for (var i = 0; i < 1000; i++) {
 		al[i].show();
 		al[i].update();
 		al[i].restore();
 	}
-
-for (var i = 0; i < 500; i++) {
-		al1[i].show();
-		al1[i].update();
-		al1[i].restore();
-	}
-
-	
-
 }
 
