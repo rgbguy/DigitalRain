1db08d9ba3926c8809ba5b437185ff96c619db08
sagarkalbande <sagarkalbande1993@gmail.com>
2018-04-28 01:10:56 +0530

    Thunder effect!

--- aliens.js
+++ aliens.js
@@ -8,18 +8,22 @@ function alien()
 	this.y = random(0,-600);
 
 
-	this.update = function(){
+	this.update = function()
+	{
 		this.y = this.y + this.speedy + 5;
 		this.x = this.x + 2;
 	}
 
-	this.show = function(){
-		fill(255);
+	this.show = function()
+	{
+		fill(255, 204, 0);
 		rect(this.x,this.y,0.5,5*this.speedy);
 	}
 
-	this.restore = function(){
-		if(this.y>windowHeight){
+	this.restore = function()
+	{
+		if(this.y>windowHeight)
+		{
 			this.y=random(0,-600);
 			this.x=random(-1000,width);
 		}
--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 var al = [];
-
+var col= 100;
+var c= 80;
+var d= 100;
 
 function setup()
 {
@@ -13,13 +15,26 @@ createCanvas(windowWidth, windowHeight);
 
 function draw()
 {
-background(135,206,250);
+
+  col = mouseX/3;
+  c,d = mouseY/3;
+  var random_boolean = Math.random() >= 0.98;
+  if (random_boolean) 
+  {
+  	background (255);
+  }
+  else
+  {
+  	  background (100,100,100);
+  }
+
 
 	for (var i = 0; i < 1000; i++) 
 	{
 		al[i].show();
 		al[i].update();
 		al[i].restore();
+
 	}
 }
 
